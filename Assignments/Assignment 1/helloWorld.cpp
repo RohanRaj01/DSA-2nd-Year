15f79054ddf2bffdb0a98d43d0a15eebbd3e26c1
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-09-20 23:26:11 +0530

    hello world program in cpp

---
+++ Assignments/Assignment 1/helloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+
+Link :  https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true

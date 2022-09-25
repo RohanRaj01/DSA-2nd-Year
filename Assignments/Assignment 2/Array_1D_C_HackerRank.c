d50c1f5f0cacdeecc73803b6f54bf83d4810be43
RohanRaj01 <107697816+RohanRaj01@users.noreply.github.com>
2022-09-25 19:21:39 +0530

    1D Array in c in Hacker rank

---
+++ Assignments/Assignment 2/Array_1D_C_HackerRank.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%d ", &arr[i]);
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+    }
+    printf("%d", sum);
+       
+    return 0;
+}
+
+
+// Link : https://www.hackerrank.com/challenges/1d-arrays-in-c/submissions/code/291557412
